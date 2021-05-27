f91a271bcdd4efeb7805a69954109f079af6d95e
Laura <law7676@outlook.com>
2021-05-27 13:31:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ keys.forEach(function(key){
 })
 
 // Write named functions that change the color of the keys below
+const keyPlay = (e) => {
 
+};
 
 // Write a named function with event handler properties
 
